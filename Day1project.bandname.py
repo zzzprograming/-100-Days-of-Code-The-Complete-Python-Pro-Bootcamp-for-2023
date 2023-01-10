34c007c1b16efda492ca54b9445ede569112f12e
ZeYu Chen <106275064+zzzprograming@users.noreply.github.com>
2023-01-10 14:43:04 -0500

    Add files via upload

---
+++ Day1project.bandname.py
@@ -0,0 +1,15 @@
+#100 Days of Code, Day 1 project
+#Band-name-generator
+#The goal is to combind name and your pet name togther
+
+#program starts
+#1. Create a greeting for your program
+print("Welcome to 100 days of Code, Day 1 project")
+#2. Ask the user for the city that they gew up in
+city = input("What city you have grew up in?\n")
+#3. Ask the suer for the name of a pet
+pet = input("What is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name
+band = city + pet
+print("Your band name is:" + band)
+
